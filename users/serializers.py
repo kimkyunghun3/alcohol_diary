17b7c8f87257d9dece034ca914b803a0dff148f4
kimkyunghun3 <kimkyunghun3@naver.com>
2020-02-08 22:24:30 +0900

    Userserializers에서 unique id값을 제외한 class 선언

--- users/serializers.py
+++ users/serializers.py
@@ -4,11 +4,20 @@ from users.models import User
 
 
 class UserSerializers(serializers.ModelSerializer):
-
     class Meta:
         model = User
         fields = (
             'name',
             'img_profile',
             'id',
+        )
+
+
+class UserSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'name',
+            'img_profile',
+
         )
